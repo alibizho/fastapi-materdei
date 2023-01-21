387dc3dc2621beb5203ba795fd17d201b687da86
alibizho <75941104+alibizho@users.noreply.github.com>
2023-01-21 17:23:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
 from scraper import Scraper
-
+from pydantic import BaseModel
 
 app = FastAPI()
 
 quotes = Scraper()
 
 
-
-@app.get('/{events}')
+@app.get('/events')
 async def read_events(year, month):
     return quotes.scrapedata(year, month)
