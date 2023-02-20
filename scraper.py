1f3e38a5521ee8956eacf2480f09be3f7eeba42d
alibizho <75941104+alibizho@users.noreply.github.com>
2023-02-20 09:27:42 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,8 +1,82 @@
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 import requests
+from itertools import zip_longest
+
 
 class Scraper():
+
+    def scrape_data(self, year, month):
+        url = f'https://www.materdei.org/apps/events/{year}/{month}/?id=6'
+
+        b = requests.get(url)
+
+        soup = BeautifulSoup(b.content, 'html.parser')
+
+        info = soup.findAll('ul', class_='day-event-box')
+        date = soup.findAll('span', class_='date')
+
+
+        def get_tables():
+
+            table_info = []
+
+
+            for i in info:
+                links = i.findAll('a', class_='event-link')
+
+                for link in links:
+                    href = link.attrs['href']
+
+                    burl = f'https://www.materdei.org{href}'
+
+                    b = requests.get(burl)
+
+                    soup = BeautifulSoup(b.content, 'html.parser')
+
+                    table = soup.find('table', class_='MsoNormalTable')
+                    semester = soup.find('p', class_='MsoNormal')
+                    no_school_event_info = soup.findAll('div', class_='event-info')
+                    for no in no_school_event_info:
+                        no_school = no.find('dd').get_text().strip()
+                        if no_school[0:9] == 'No School':
+                            table_info.append('There is no table for this event!')
+
+                    if table:
+                        dable = soup.findAll('p', class_='MsoNormal')
+                        texts = []
+                        for text in dable:
+                            texts.append(text.text)
+                        table_info.append(texts)
+                    elif semester:
+                        event_info = soup.findAll('div', class_='event-info')
+                        for me in event_info:
+                            x = me.find('p')
+                            xx = x.get_text(separator='   ').strip()
+                            table_info.append(xx)
+
+
+            return table_info
+
+
+
+
+        tables = get_tables()
+        eventlist = []
+
+        for i, d, t in zip_longest(info, date, tables, fillvalue='There is nothing'):
+            item = {
+                'Date': d.text,
+                'Block': i.find('a', class_='event-link').text.strip(),
+                'Table': t,
+                'Month': month,
+                'Year': year
+            }
+
+            eventlist.append(item)
+
+        return eventlist
+
 
     def scrape_dress(self,year, month):
 
